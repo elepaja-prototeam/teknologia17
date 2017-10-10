19974476e94f3670ee9d1925dc3215ab1f787fc1
Sampsa Penna <sampsapenna@gmail.com>
2017-10-10 10:34:37 +0300

    Added some comments

--- serial_data_plot_example.py
+++ serial_data_plot_example.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 
+# imports, as in what libraries to use
 import serial
 import sys
 import os
 import time
 
 
+# Constants
 QUERY = 'A'
 FILEPATH = '/var/www/html/'
 FILENAME = dataplot.csv
@@ -14,6 +16,8 @@ SERIALPATH = "/dev/serial0"
 
 
 # serial_init initializes serial with the correct device, specified in the constant SERIALPATH
+# baudrate of 9600 is a commonly used speed for the serial connection, and should be safe to
+# use, even when the wires connecting the two ports are not of the highest quality.
 def serial_init():
     port = serial.Serial(SERIALPATH, baudrate=9600, timeout=3.0)
     return port
@@ -26,8 +30,10 @@ def serial_query(port, query):
     return ret
 
 
+# writes the data collected from serial into a .csv file, to be read into an external
+# plotting program, eg. a javascript based interactive website
 def plot_to_csv(datal):
-    try:
+    try: # try opening the file and write values into it
         filename = os.path.abspath(FILEPATH + FILENAME)
         file = open(filename, 'w')
         file.write("time,value")
@@ -39,6 +45,12 @@ def plot_to_csv(datal):
         print("Error writing to file, check permissions and lsof")
 
 
+# function to read values for a specified time, from the serial port.
+# Function saves the starting time into a variable, to check how much
+# time has passed since last iteration. When the amount specified in
+# the variable 'length' if filled, the loop ends and returns all of
+# the values that were read while in the loop.
+# Format for the values is [[time,value],[time,value]...[time,value]]
 def read_values(port, length):
     begint = time.time()
     datal = []
@@ -51,9 +63,14 @@ def read_values(port, length):
     return datal
 
 
+# If the program is run as a __main__ module, run main; otherwise
+# complain about not being run as main.
 if __name__ == '__main__':
     try:
         main()
         sys.exit(0)
     except KeyboardInterrupt:
         sys.exit(0)
+else:
+    print("This program has to be run as main!")
+    sys.exit(-1)
